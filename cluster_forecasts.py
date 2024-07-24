3d0981335c93bb05229e233177d7144db2fd5bcd
SohamX1999 <163124500+SohamX1999@users.noreply.github.com>
2024-07-24 09:46:42 +0530

    Update cluster_forecasts.py

--- cluster_forecasts.py
+++ cluster_forecasts.py
@@ -3,40 +3,13 @@ import pandas as pd
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
-import gdown
-
-# File ID from the Google Drive shareable link
-file_id = '1sk2yygfI-cYunRJ4AelViKirNbBtbnkY'
-
-# Create the download URL
-url = f'https://drive.google.com/uc?id={file_id}'
-
-# Download the file
-output = 'sales_store_promotions.csv'
-gdown.download(url, output, quiet=False)
-
-# Read the CSV file into a DataFrame
-df = pd.read_csv(output)
-
-promo_features = ['PLATFORM_DD',
-       'PLATFORM_Inhouse', 'PLATFORM_UE', 'FREQUENCY_Weekday', 'PromotionCash',
-       'PromotionKind', 'PROMOTION_ITEM_DeliveryFee',
-       'PROMOTION_COVERAGE_Regional']
-
-
-df[promo_features] = df[promo_features].fillna(0)
-df[promo_features] = df[promo_features].astype(int)
-df['BUSINESS_DATE'] = pd.to_datetime(df['BUSINESS_DATE'], format = '%d-%m-%Y')
-
-# Forecast only for stores that we have enough data on (at least 90 days)
-stores = df[df['BUSINESS_DATE'] <= '2023-07-04']['STORE_KEY'].unique()
-df = df[df['STORE_KEY'].isin(stores)]
 
 # Load the pickle files
 with open('store_forecasts.pkl', 'rb') as f:
     store_forecasts = pickle.load(f)
 
-
+with open('actual_sales.pkl', 'rb') as f:
+    actual_sales_dict = pickle.load(f)
 
 def main():
     st.title("Sales Forecasting")
@@ -49,7 +22,7 @@ def main():
             st.write(f"Forecasted Sales for Store Key: {store_key}")
 
             # Extract actual sales for the selected store
-            actual_sales = df[df['STORE_KEY'] == store_key].set_index('BUSINESS_DATE')['NET_SALES_FINAL_USD_AMOUNT'].sort_index()[-90:]
+            actual_sales = actual_sales_dict[store_key][-90:]
 
             # Extract forecasted sales for the selected store
             forecasted_sales = forecast_df['Forecast_Sales']
